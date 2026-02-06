181e6df4946becf5a9bb1917c3a1f3d9c10a3be6
y9828 <y9828@naver.com>
2026-02-06 18:12:00 +0900

    [수정] bulb 수정

--- section05/src/components/Bulb.jsx
+++ section05/src/components/Bulb.jsx
@@ -1,25 +1,20 @@
 import { useState } from "react";
 
 const Bulb = ()=>{
-  const onLightClick = (e) => {setLight(light === 'ON' ? 'OFF': 'ON')}
+  const [light, setLight] = useState('ON');
 
-
+  const onLightClick = (e)=>{ setLight(light === 'ON' ? 'OFF': 'ON')}
 
   return <>
-  
   <div>
-    {light === 'ON' ?(
-<h1 style={{backgroundColor:'orange'}}>Bulb 컴포 ON</h1>
+    {light === 'ON' ? (
+      <h1 style={{backgroundColor: 'orange'}}>Bulb ON</h1>
     ):(
-<h1 style={{backgroundColor:'blue'}}>Bulb 컴포 OFF</h1>
-
-
+      <h1 style={{backgroundColor: 'gray'}}>Bulb OFF</h1>
     )}
-    <p>rander {console.log('rander')}</p>
-    <button type="button" onClick={onLightClick}>{light === 'ON' ? 'OFF': 'ON'}</button>
-    
+   
+     <button type="button" onClick={onLightClick}>{light==='ON'?'끄기':'켜기'}</button>
   </div>
   </>
 }
-
-export default Bulb;
+export default Bulb; 

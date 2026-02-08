1d46efb7bd0ea16d9062876818812756b3588168
y9828 <y9828@naver.com>
2026-02-08 17:55:16 +0900

    [연습] 자바스크립트 콜백함수

---
+++ section01/chapter01/chapter13_02.js
@@ -0,0 +1,16 @@
+function repeat(count, callback) {
+ for (let idx = 1; idx <= count; idx++) {
+ callback(idx);
+ }
+}
+
+
+repeat(5, (idx) => {
+ console.log(idx);
+});
+repeat(5, (idx) => {
+ console.log(idx * 2);
+});
+repeat(5, (idx) => {
+ console.log(idx * 3);
+});

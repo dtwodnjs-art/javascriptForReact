8fb4fd22001ee64f239a72817be3d6fae34aac97
y9828 <y9828@naver.com>
2026-03-19 14:02:57 +0900

    [생성] 260319 수업내용

--- mall/src/slices/loginSlice.js
+++ mall/src/slices/loginSlice.js
@@ -1,18 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { loginPost } from "../api/memberApi";
+import { build } from "vite";
 
 const initState = {
   email: "",
 };
+
+export const loginPostAsync = createAsyncThunk(`loginPostAsync`, (param) => {
+  return loginPost(param);
+});
+
 const loginSlice = createSlice({
   name: "LoginSlice",
   initialState: initState,
   reducers: {
     login: (state, action) => {
-      console.log("로그인 .................. ");
+      console.log("로그인 ");
+      //{email, pw 로 구성 }
+      const data = action.payload;
+      //새로운 상태
+      return { email: data.email };
     },
     logout: (state, action) => {
       console.log("로그아웃 ............... ");
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginPostAsync.fulfilled, (state, action) => {})
+      .addCase(loginPostAsync.pending, (state, action) => {})
+      .addCase(loginPostAsync.rejected, (state, action) => {});
   },
 });
 export const { login, logout } = loginSlice.actions;
